6ddf7663b7884f38faf7e7859083d320642441ee
Sean Washington <hello@seanwash.com>
2024-04-30 21:31:25 -0700

    Snapshot

--- src/lib/server/pb.ts
+++ src/lib/server/pb.ts
@@ -2,6 +2,8 @@ import PocketBase from 'pocketbase';
 import { PB_URL, PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD } from '$env/static/private';
 
 const pb = new PocketBase(PB_URL);
-await pb.admins.authWithPassword(PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD);
+await pb.admins.authWithPassword(PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD, {
+	autoRefreshThreshold: 30 * 60
+});
 
 export default pb;
